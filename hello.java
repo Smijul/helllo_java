99319e672d6a4f2496a5589a32658482902132df
Smijul <109330131+Smijul@users.noreply.github.com>
2023-05-19 07:16:15 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! haaaaaaaai waaaaaaaooooowww"); 
+        System.out.println("Hello, World! haaaaaaaai waaaaaaaooooowww hello boi i made chandes"); 
     }
 }
